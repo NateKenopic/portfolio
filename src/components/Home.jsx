2f34ffe3eb3da0df4f07d0c3ea81ba3b058f4a20
NateKenopic <nkenopic@outlook.com>
2022-10-13 09:30:34 -0400

    Updated Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             <h1 className='text-5xl lg:text-8xl font-bold text-[#ccd6f6]'>Nathan Kenopic</h1>
             <h2 className='text-4xl lg:text-7xl font-bold text-slate-400 pt-2'>Full Stack & Software developer</h2>
             <p className='text-slate-400 py-5 max-w-[650px] lg:text-2xl'>I'm a software developer and a full stack developer focusing on building apps, games, and great responsive
-                 websites with both front-end and both-end technologies.
+                 websites with both front-end and back-end technologies.
             </p>
             <div>
                 <button className='group flex items-center text-white border-2 border-blue-400 px-5 py-2 hover:bg-[#ff0000] hover:border-[#ff0000] hover:duration-300'> 
