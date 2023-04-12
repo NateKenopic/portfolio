66ca313d7bf84b05ea2e30decd4d325dc6917237
Nate-K0 <nkenopic@myseneca.ca>
2023-04-12 00:48:58 -0400

    Updated Portfolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
-import { HiArrowNarrowRight } from 'react-icons/hi'
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className='text-[#ff0000] lg:text-2xl'>Hello!&nbsp;&nbsp; My name is</p>
-            <h1 className='text-5xl lg:text-8xl font-bold text-[#ccd6f6]'>Nathan Kenopic</h1>
-            <h2 className='text-4xl lg:text-7xl font-bold text-slate-400 pt-2'>Full Stack & Software developer</h2>
-            <p className='text-slate-400 py-5 max-w-[650px] lg:text-2xl'>I'm a software developer and a full stack developer focusing on building apps, games, and great responsive
-                 websites with both front-end and back-end technologies.
-            </p>
-            <div>
-                <button className='group flex items-center text-white border-2 border-blue-400 px-5 py-2 hover:bg-[#ff0000] hover:border-[#ff0000] hover:duration-300'> 
-                    See My Work 
-                    <span>
-                        <HiArrowNarrowRight className='ml-2'/> 
-                    </span>
-                </button>
-            </div>
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-[#ff0000]'>Hello!&nbsp;&nbsp; My name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+            Nathan Kenopic
+        </h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+            Full Stack & Software Developer
+        </h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+            I'm a software developer and a full stack developer focusing on building apps, games, and great responsive
+            websites with both front-end and back-end technologies.
+        </p>
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+          <Link to='work' smooth={true} duration={500}>
+            See My Projects
+          </Link>
+            <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight className='ml-3 ' />
+            </span>
+          </button>
         </div>
+      </div>
     </div>
   )
 }
---
+++ src/data/projects.js
@@ -0,0 +1,11 @@
+import SeneConnect from '../assets/seneconnect.png'
+
+export const projects=[
+    {
+        id:1,
+        name:"SeneConnect",
+        image:SeneConnect,
+        github:"https://github.com/Nate-K0/SeneConnectApp",
+        live:"https://seneconnect.vercel.app/login",
+    },
+]
