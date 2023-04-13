3ab6d8fec967e55ff08c385063d4ae7ffd2e7f3c
Nate-K0 <nkenopic@myseneca.ca>
2023-04-12 20:27:39 -0400

    Updated Portfolio

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,6 @@
 import SeneConnect from '../assets/seneconnect.png'
 import MovieFinder from '../assets/movie_finder.png'
+import PyHeart from '../assets/pyheart.png'
 
 export const projects=[
     {
@@ -15,5 +16,12 @@ export const projects=[
         image:MovieFinder,
         github:"https://github.com/NateKenopic/movie_finder_app",
         live:"https://nathankenopic-moviefinder.netlify.app/",
+    },
+    {
+        id:3,
+        name:"PyHeart",
+        image:PyHeart,
+        github:"https://github.com/NateKenopic/pyheart",
+        live:"",
     },
 ]
