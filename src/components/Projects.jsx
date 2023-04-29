3c24daa2202d5921f6c9218f69b92165cf641bef
NateKenopic <nkenopic@outlook.com>
2023-04-29 15:18:32 -0400

    updated portfolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,41 +14,41 @@ const Projects = () => {
           <p className='py-6'>Check out some of my recent work</p>
         </div>
 
-  <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-    {project.map((item, index) => (
-    <div
-      key={index}
-      style={{ backgroundImage: `url(${item.image})` }}
-      className="shadow-lg shadow-[#040c16] group container rounded-md 
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {project.map((item, index) => (
+            <div
+              key={index}
+              style={{ backgroundImage: `url(${item.image})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md 
                 flex justify-center text-center items-center mx-auto content-div ">
 
-      <div className="opacity-0 group-hover:opacity-100">
-        <span className="text-2xl font bold text-white tracking-wider ">
-          {item.name}
-        </span>
-        <div className="pt-8 text-center ">
-          <a href={item.github} target="blank">
-            <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-              GitHub
-            </button>
-          </a>
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font bold text-white tracking-wider ">
+                  {item.name}
+                </span>
+                <div className="pt-8 text-center ">
+                  <a href={item.github} target="blank">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      GitHub
+                    </button>
+                  </a>
 
-          {item.live !== "" ?
-          <a href={item.live} target='blank'>
-            <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-              Live
-            </button>
-          </a>
-          : <></>
-          }
+                  {item.live !== "" ?
+                    <a href={item.live} target='blank'>
+                      <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                        Live
+                      </button>
+                    </a>
+                    : <></>
+                  }
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-  </div>
-  ))}
-  </div>
-      </div>
     </div>
-    )
+  )
 }
 
 export default Projects
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,10 @@
 import SeneConnect from '../assets/seneconnect.png'
 import MovieFinder from '../assets/movie_finder.png'
-import PyHeart from '../assets/pyheart.png'
+// import PyHeart from '../assets/pyheart.png'
 import ChatApp from '../assets/chat-app.png'
 import ThreeDWebsite from '../assets/3d-website.png'
 import BankApp from '../assets/bank-app.png'
+import ThreeDCar from '../assets/3d-car-site.png'
 
 export const projects=[
     {
@@ -20,32 +21,39 @@ export const projects=[
         github:"https://github.com/NateKenopic/movie_finder_app",
         live:"https://nathankenopic-moviefinder.netlify.app/",
     },
+    // {
+    //     id:3,
+    //     name:"PyHeart",
+    //     image:PyHeart,
+    //     github:"https://github.com/NateKenopic/pyheart",
+    //     live:"",
+    // },
     {
         id:3,
-        name:"PyHeart",
-        image:PyHeart,
-        github:"https://github.com/NateKenopic/pyheart",
-        live:"",
-    },
-    {
-        id:4,
         name:"Chat App",
         image:ChatApp,
         github:"https://github.com/NateKenopic/chat-app",
         live:"",
     },
     {
-        id:5,
+        id:4,
         name:"Interactive 3D Website",
         image:ThreeDWebsite,
         github:"https://github.com/NateKenopic/3d-website",
         live:"https://nathankenopic-3d-sphere.netlify.app/",
     },
     {
-        id:6,
+        id:5,
         name:"Python Bank App",
         image:BankApp,
         github:"https://github.com/NateKenopic/bank_app",
         live:"",
+    },
+    {
+        id:6,
+        name:"Interactive 3D Car",
+        image:ThreeDCar,
+        github:"https://github.com/NateKenopic/3d-car",
+        live:"https://nathankenopic-3d-car.netlify.app/",
     },
 ]
